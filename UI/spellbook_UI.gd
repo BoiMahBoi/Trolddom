6d5361d1d5907e096add7287f761013658470a74
BoiMahBoi <mtr0607@hotmail.dk>
2024-07-26 16:34:13 +0200

    spellbookUpdate

---
+++ UI/spellbook_UI.gd
@@ -0,0 +1,21 @@
+extends Control
+
+var pages : Array = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func update_page_array():
+	pages[0] = %EquipmentPage
+	pass
+
+func generate_page():
+	pass
+
+func get_owned_spells():
+	pass
